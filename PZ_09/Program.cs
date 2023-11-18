b6240fbce1cfe58d053e85e7fd1f7262768a77fc
Mir Nekit <mirmirnekitmir25@gmail.com>
2023-11-18 23:35:43 +0500

    Добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string maxString = "";
+
+            Console.WriteLine("Введите строки. 'stop' для завершения");
+
+            while (true)
+            {
+                Console.WriteLine("Введите строку: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput.ToLower() == "stop")
+                    break;
+
+                if (userInput.Length > maxString.Length)
+                    maxString = userInput;
+            }
+
+            if (!string.IsNullOrEmpty(maxString))
+                Console.WriteLine($"Строка max длинны: {maxString}");
+            else
+                Console.WriteLine("Ни одной строки не введено");
+        }
+    }
+}
