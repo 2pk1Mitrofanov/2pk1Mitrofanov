72c4f87f69dc80cf03ccec26073a2122afcf9ea0
Mir Nekit <mirmirnekitmir25@gmail.com>
2023-12-01 16:37:10 +0500

    Добавлена 11 и 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 7, 5, 2, 3, 4, 8, 1, 16 };
+            Array.Sort(numbers);
+
+            double median = CalculateMedian(numbers);
+
+            Console.WriteLine($"Медиана равна {median}");
+        }
+        static double CalculateMedian(int[] sortedArray)
+        {
+            int n = sortedArray.Length;
+
+            if (n % 2 == 0 )
+            {
+                int middleIndex1 = n / 2 - 1;
+                int middleIndex2 = n / 2;
+                return (sortedArray[middleIndex1] + sortedArray[middleIndex2]) / 2.0;
+            }
+            else
+            {
+                int middleIndex = n / 2;
+                return sortedArray[middleIndex];
+            }
+        }
+    }
+}
---
+++ pz_11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = { 3.5, 4.0, 5.2, 6.7, 7.1 };
+
+            foreach (var number  in numbers)
+            {
+                double result;
+                PowerA3(number, out result);
+                Console.WriteLine($"3 степень числа {number}: {result}") ;
+            }
+        }
+        static void PowerA3(double A, out double B)
+        {
+            B = Math.Pow(A, 3);
+        }
+    }
+}
