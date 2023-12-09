059295ab57f8629fcdb99a193a2366c93ea086fb
Mir Nekit <mirmirnekitmir25@gmail.com>
2023-12-09 14:50:34 +0500

    Добавлена 15 практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PZ_15;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите полный путь к каталогу: ");
+        string directoryPath = Console.ReadLine();
+
+        try
+        {
+            ListFilesWithLatinNames(directoryPath);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+
+        Console.ReadLine();
+    }
+
+    static void ListFilesWithLatinNames(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            throw new DirectoryNotFoundException($"Каталог {directoryPath} не существует.");
+        }
+
+        Console.WriteLine($"Файлы в каталоге {directoryPath}, имена которых написаны латиницей:");
+
+
+        string[] files = Directory.GetFiles(directoryPath);
+
+
+        foreach (string filePath in files)
+        {
+
+
+            try
+            {
+                string fileName = Path.GetFileName(filePath);
+
+
+                // Проверка, содержит ли имя файла только латинские символы
+                if (IsLatinName(fileName))
+                {
+                    Console.WriteLine(fileName);
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при обработке файла: {ex.Message}");
+            }
+        }
+    }
+
+    static bool IsLatinName(string fileName)
+    {
+
+        // Регулярное выражение для проверки, содержит ли строка только латинские символы
+        Regex regex = new Regex("^[a-zA-Z]+$");
+
+        if (!regex.IsMatch(fileName))
+        {
+            throw new Exception($"Имя файла {fileName} содержит нелатинские символы.");
+        }
+
+        return true;
+    }
+}
